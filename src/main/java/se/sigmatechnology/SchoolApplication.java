0079afb2cd2776a86ec85e32711d502a23e13b78
Pezhman Kasraee <pezhman.kasraee@sigmatechnology.se>
2017-09-01 15:13:45 +0200

    ADD FEAT: pom is updated by spring and junit4

---
+++ src/main/java/se/sigmatechnology/SchoolApplication.java
@@ -0,0 +1,16 @@
+package se.sigmatechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SchoolApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
+}
